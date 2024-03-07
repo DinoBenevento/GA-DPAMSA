b4ad540ae773b38afdf260c1a06ddf780fa9febf
Gabriele <gabrieletuozzo@gmail.com>
2024-03-07 11:24:53 +0000

    updated dataset3.py

--- dataset3.py
+++ dataset3.py
@@ -1,7 +1,7 @@
 file_name = './test1'
 datasets = ['test1']
 test1 = [
-    "AGCTAGCTAGCT"
-    "AGCTAGCTAGCTAGCT"
-    "AGCTAGCTGCTGCT"
+    "AGCTAGCTAGCT",
+    "AGCTAGCTAGCTAGCT",
+    "AGCTAGCTGCTGCT",
 ]
