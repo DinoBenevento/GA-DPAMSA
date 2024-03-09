9eca13430221b44b41973fe5470661254c1d4a75
Gabriele <gabrieletuozzo@gmail.com>
2024-03-09 09:36:39 +0000

    added two dataset

---
+++ datasets/dataset3.py
@@ -0,0 +1,16 @@
+file_name = './test1'
+datasets = ['test1','test2']
+
+test1 = [
+    "AGCTAGCTAGCT",
+    "AGCTAGCTAGCTAGCT",
+    "AGCTAGCTGCTGCT",
+]
+
+'''
+test2 = [
+    "TTTTAGCTAGCTAGCTTTTTAGCTAGCTAGCT",
+    "TTTTAGCTAGCTAGCT",
+    "TTTTAGCTAGCTAGCT",
+]
+'''
