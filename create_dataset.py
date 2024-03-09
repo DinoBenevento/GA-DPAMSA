f5503f16c64555e7ce4a9779591962b169cdb42a
Gabriele <gabrieletuozzo@gmail.com>
2024-03-09 09:36:16 +0000

    added script to create some dataset to test the code

---
+++ create_dataset.py
@@ -0,0 +1,30 @@
+import random
+
+BP = 20
+NUM_SEQUENCES = 5
+NUM_DATASET = 20
+FILE_NAME_REPORT = './dataset1'
+FILE_NAME_SCRIPT_OUTPUT = './datasets/dataset1.py'
+nucleotides_map = {'A': 1, 'T': 2, 'C': 3, 'G': 4}
+
+datasets = []
+for i in range(NUM_DATASET):
+    sequences = []  
+    for j in range(NUM_SEQUENCES):
+        sequence = ''
+        for k in range(BP):
+          sequence = sequence + random.choice(list(nucleotides_map.keys()))
+        sequences.append(sequence)
+    datasets.append(sequences)
+
+with open(FILE_NAME_SCRIPT_OUTPUT, 'w') as file:
+    file.write(f"file_name = '{FILE_NAME_REPORT}'\n\n")
+    file.write(f"datasets = [")
+    for index,dataset in enumerate(datasets):
+        file.write(f"'dataset{index}',")
+    file.write("]\n\n")
+    for index,dataset in enumerate(datasets):
+        file.write(f"dataset{index} = [\n")
+        for sequence in dataset:
+            file.write(f'        "{sequence}",\n')
+        file.write("    ]\n\n")
